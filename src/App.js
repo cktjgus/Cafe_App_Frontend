053b8d8e26a63c8096a77c0b6c340550e23778fc
cktjgus <157617108+cktjgus@users.noreply.github.com>
2025-05-28 14:59:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import KakaoMap from "./components/KakaoMap";
-import Header from "./components/Header";
-import CafeList from "./components/CafeList";
-import SeatList from "./components/SeatList";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import KakaoMap from './components/KakaoMap';
+import Header from './components/Header';
+import CafeList from './components/CafeList';
+import SeatList from './components/SeatList';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route
           path="/map"
           element={
